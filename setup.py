63dd7f98df9fddd6584a1920233daf58cf44804c
Dan Marshall <danmar@microsoft.com>
2025-05-09 10:54:08 -0700

    Bump version to 0.1.1 in pyproject.toml and setup.py; add long description to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@
 # Licensed under the MIT License.
 
 from setuptools import setup, find_packages
+from pathlib import Path
 
 setup(
     name="notebook_nanoservice",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="."),
     author="Your Name",
     author_email="danmar@microsoft@example.com",
     description="Nanoservices for notebooks",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
     url="https://github.com/microsoft/notebook-nanoservice"
 )
